dbb057f441cb27cd91e5bdecc97574823daccfd5
ramana-008 <ramana.u2808@gmail.com>
2026-02-26 11:53:22 +0530

    version 2

--- testnew.java
+++ testnew.java
@@ -1,8 +1,8 @@
 public class testnew {
     public static void main(String[] args) {
-        // Loop to print "Hello World" five times
+        // Loop to print "IVam sleeping" five times
         for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello World");
+            System.out.println("I am sleeping");
         }
     }
 }
