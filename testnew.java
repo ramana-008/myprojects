2835b3bd6ed87b313297e2e8012a4acda62ee3a3
ramana-008 <ramana.u2808@gmail.com>
2026-02-26 10:20:33 +0530

    my first project in java

---
+++ testnew.java
@@ -0,0 +1,8 @@
+public class testnew {
+    public static void main(String[] args) {
+        // Loop to print "Hello World" five times
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
