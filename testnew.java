3c84524f514a28aa4c888fbaee49be1922500d8c
ramana-008 <ramana.u2808@gmail.com>
2026-02-26 11:56:47 +0530

    Updat

--- testnew.java
+++ testnew.java
@@ -1,8 +1,11 @@
 public class testnew {
     public static void main(String[] args) {
         // Loop to print "IVam sleeping" five times
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("I am sleeping");
+        for (int i = 1; i <= 8; i++) {
+            System.out.println("I like ash");
+                            
+
+                              
         }
     }
 }
